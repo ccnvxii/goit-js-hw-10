ef8e49c832c2cb92d2a49de9a55e5202823cbb10
ccnvxii <73746043+ccnvxii@users.noreply.github.com>
2026-02-10 13:16:14 +0000

    Deploying to gh-pages from @ ccnvxii/goit-js-hw-10@9e6eb276037cc82d0294dc1de741c9f1959690d7 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const e=document.querySelector(".form");e.addEventListener("submit",t=>{t.preventDefault()});
+//# sourceMappingURL=2-snackbar.js.map

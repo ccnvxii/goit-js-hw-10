936d3d051cad1a1b50a5ee0b3b1cbdb304d1a1c3
ccnvxii <dasharossoha16@gmail.com>
2026-02-11 14:23:44 +0200

    second ex

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -7,4 +7,29 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
 });
